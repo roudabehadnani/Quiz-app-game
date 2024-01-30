28c43da7f1808e0bc15647c54a991e4f8e94c482
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2024-01-30 14:05:00 +0100

    add next button function to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,65 @@
-import { useEffect, useReducer } from 'react';
-import Header from './Header';
-import Main from './Main';
-import LoadingLayout from './LoadingLayout';
-import ErrorLayout from './ErrorLayout';
-import StartScreen from './StartScreen';
-import Question from './Question';
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import LoadingLayout from "./LoadingLayout";
+import ErrorLayout from "./ErrorLayout";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
 
 const initialState = {
-  questions:[],
+  questions: [],
 
   // "loading","error", "ready", "active", "finished"
-  status:"loading",
-  index:0,
+  status: "loading",
+  index: 0,
   answer: null,
-  points:0
+  points: 0,
 };
-function reducer(state, action){
-  switch(action.type){
+function reducer(state, action) {
+  switch (action.type) {
     case "dataReceived":
-      return {...state, questions: action.payload , status:"ready"};
+      return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
-      return{...state, status:"error"};
+      return { ...state, status: "error" };
     case "start":
-      return{...state, status:"active"}
+      return { ...state, status: "active" };
     case "newAnswer":
-      const question = state.questions.at(state.index)
-      return{...state, answer:action.payload, points:action.payload === question.correctOption ? state.points + question.points : state.points}
+      const question = state.questions.at(state.index);
+      return { ...state, answer: action.payload, points: action.payload === question.correctOption ? state.points + question.points : state.points };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1 , answer: null};
     default:
       throw new Error("Unknown action");
-    } 
+  }
 }
 
 function App() {
-  
-  const [{questions, status, index, answer, points}, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState);
   const numQuestion = questions.length;
-  console.log(numQuestion)
+  console.log(numQuestion);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch("http://localhost:3001/questions")
-    .then(res =>res.json())
-    .then(data=>dispatch({type:'dataReceived', payload:data}))
-    .catch(err=>dispatch({type:'dataFailed'}))
-  },[])
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
 
   return (
-    <div className='app'>
-      <Header/>
+    <div className="app">
+      <Header />
       <Main>
-        {status === "loading" && <LoadingLayout/>}
-        {status === "error" && <ErrorLayout/>}
-        {status === "ready" && <StartScreen numQuestion={numQuestion} dispatch={dispatch}/>}
-        {status === "active" && <Question question={questions[index]} answer={answer} dispatch={dispatch}/>}
+        {status === "loading" && <LoadingLayout />}
+        {status === "error" && <ErrorLayout />}
+        {status === "ready" && <StartScreen numQuestion={numQuestion} dispatch={dispatch} />}
+        {status === "active" && (
+          <>
+            <Question question={questions[index]} answer={answer} dispatch={dispatch} /> <NextButton dispatch={dispatch} answer={answer} index={index} points={points} />
+          </>
+        )}
       </Main>
     </div>
-
   );
 }
 
